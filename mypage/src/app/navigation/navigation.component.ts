f319059bab35196092ccec71eb57041c7b1b7fcf
Norman Yan <hwyan0220@gmail.com>
2018-12-19 22:17:52 +1100

    Removed core folder and updated more contents

---
+++ mypage/src/app/navigation/navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material';
+
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+})
+export class NavigationComponent {
+
+@ViewChild('drawer') public drawer: MatDrawer;
+
+  constructor() {}
+
+navScroll(target) {
+  this.drawer.close();
+  document.getElementById(target).scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+}
