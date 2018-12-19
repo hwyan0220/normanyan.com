198f80324e0b5064ed397a85920096aaef7128da
Norman Yan <hwyan0220@gmail.com>
2018-12-19 22:17:00 +1100

    added mat-grid-list and mat-card module to app.module

--- mypage/src/app/app.module.ts
+++ mypage/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './core/navigation/navigation.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule} from '@angular/material';
+import { 
+  MatToolbarModule, MatButtonModule, MatSidenavModule,
+  MatIconModule, MatListModule, MatGridListModule, MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatGridListModule
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
