1f95c06b6f822de3b05ecaae33e02e9e39ea1667
Norman Yan <hwyan0220@gmail.com>
2019-01-04 21:45:16 +1100

    Thoughts section updated

--- mypage/src/app/thoughts/thoughts.component.ts
+++ mypage/src/app/thoughts/thoughts.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThoughtsComponent implements OnInit {
 
+  blogs =
+  [
+    {
+      title: 'End-to-end Machine Learning Project Template — Regression',
+      url: '',
+      img: 'http://www.healthcatalyst.com/wp-content/uploads/2017/10/machine-learning-in-healthcare.jpg',
+      desc: '',
+    },
+    {
+      title: 'Exploring Relationship between “Quality of Life” Factors and Sydney Property Prices',
+      url: '',
+      img: 'https://cdn.theatlantic.com/assets/media/img/mt/2018/06/GettyImages_81100278/lead_720_405.jpg?mod=1533691460',
+      desc: '',
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
