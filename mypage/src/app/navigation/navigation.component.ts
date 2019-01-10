8dee31e34021d78737d9342bd7cfea9a8cbe05e5
Norman Yan <hwyan0220@gmail.com>
2019-01-10 14:01:47 +1100

    Added btns to scroll between sections

--- mypage/src/app/navigation/navigation.component.ts
+++ mypage/src/app/navigation/navigation.component.ts
@@ -40,4 +40,39 @@ navScroll(target) {
   this.drawer.close();
   document.getElementById(target).scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+btnScroll(id) {
+  const btnPos = document.getElementById('mainBody').scrollTop+1;
+  const sections = ['greeting', 'about', 'journey', 'projects', 'thoughts', 'contacts'];
+  let btnIdx;
+
+
+  if (btnPos < document.getElementById(sections[sections.length-1]).offsetTop) {
+    for ( let i = 0; i < sections.length; i++) {
+      if (btnPos < document.getElementById(sections[i]).offsetTop) {
+        btnIdx = i;
+        console.log('btnPos is: ' + btnPos);
+        console.log('next section is: ' + document.getElementById(sections[i]).offsetTop);
+        console.log(btnIdx);
+        break;
+      } else {
+        continue;
+      }
+    }
+  } else {
+    btnIdx = sections.length;
+  }
+
+  if (id === 'upBtn') {
+    this.navScroll(sections[ btnIdx - 2 ]);
+  } else {
+    this.navScroll(sections[btnIdx]);
+  }
+
+
+}
+
 }
